226c166206811061a5571032448d44cd67dbc5c0
CsLsj <lsj001018@naver.com>
2024-07-20 10:20:01 +0900

    feat : 회원가입 zustand 도입

---
+++ src/_store/register.ts
@@ -0,0 +1,70 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface IRegisterUserState {
+  allAgree: boolean;
+  serviceAgree: boolean;
+  privateInformationAgree: boolean;
+  marketingAgree: boolean;
+  nickname: string;
+  email: string;
+  provider: string;
+  providerId: string;
+  gender: string;
+  genderCheck: boolean;
+  preferredAlcoholType: string[];
+  setAllAgree: (value: boolean) => void;
+  setServiceAgree: (value: boolean) => void;
+  setPrivateInformationAGree: (value: boolean) => void;
+  setMarketingAgree: (value: boolean) => void;
+  setNickname: (value: string) => void;
+  setEmail: (value: string) => void;
+  setProvider: (value: string) => void;
+  setProviderId: (value: string) => void;
+  setGender: (value: string) => void;
+  setGendercheck: (value: boolean) => void;
+  setPreferredAlcoholType: (value: string[]) => void;
+}
+
+export const useRegisterStore = create(
+  persist<IRegisterUserState>(
+    (set) => ({
+      allAgree: false,
+      serviceAgree: false,
+      privateInformationAgree: false,
+      marketingAgree: false,
+      nickname: "",
+      email: "",
+      provider: "",
+      providerId: "",
+      gender: "",
+      genderCheck: false,
+      preferredAlcoholType: [],
+      setAllAgree: (value: boolean) =>
+        set((state) => ({ ...state, allAgree: value })),
+      setServiceAgree: (value: boolean) =>
+        set((state) => ({ ...state, serviceAgree: value })),
+      setPrivateInformationAGree: (value: boolean) =>
+        set((state) => ({ ...state, privateInformationAgree: value })),
+      setMarketingAgree: (value: boolean) =>
+        set((state) => ({ ...state, marketingAgree: value })),
+      setNickname: (value: string) =>
+        set((state) => ({ ...state, nickname: value })),
+      setEmail: (value: string) => set((state) => ({ ...state, email: value })),
+      setProvider: (value: string) =>
+        set((state) => ({ ...state, provider: value })),
+      setProviderId: (value: string) =>
+        set((state) => ({ ...state, providerId: value })),
+      setGender: (value: string) =>
+        set((state) => ({ ...state, gender: value })),
+      setGendercheck: (value: boolean) =>
+        set((state) => ({ ...state, genderCheck: value })),
+      setPreferredAlcoholType: (value: string[]) => {
+        set((state) => ({ ...state, preferredAlcoholType: value }));
+      },
+    }),
+    {
+      name: "userRegisterStorage",
+    },
+  ),
+);
