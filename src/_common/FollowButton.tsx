47c0dd7f8c783f784a3244d1b429ee71c5790c3a
CsLsj <lsj001018@naver.com>
2024-07-29 15:58:16 +0900

    feat : Follow 페이지 구현

---
+++ src/_common/FollowButton.tsx
@@ -0,0 +1,31 @@
+interface FollowButtonProps {
+  width?: string;
+  textSize: string;
+  isFollowed: boolean;
+  onChangeFollow: () => void;
+}
+
+export default function FollowButton({
+  width,
+  textSize,
+  isFollowed,
+  onChangeFollow,
+}: FollowButtonProps) {
+  return isFollowed ? (
+    <button
+      type="button"
+      className={`mr-[4%] flex h-9 ${width ? "w-full" : "w-[91%]"} items-center justify-center rounded-[4px] border-[1px] border-cool-grayscale-300 bg-white p-2 text-black text-${textSize} font-bold`}
+      onClick={onChangeFollow}
+    >
+      팔로우 취소
+    </button>
+  ) : (
+    <button
+      type="button"
+      className={`mr-[4%] flex h-9 ${width ? "w-full" : "w-[91%]"} items-center justify-center rounded-[4px] bg-black p-2 text-white text-${textSize} font-bold`}
+      onClick={onChangeFollow}
+    >
+      팔로우 하기
+    </button>
+  );
+}
---
+++ src/app/(view)/follow/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import Loading from "@/_common/Loading";
+import FollowHeader from "@/_components/follow/FollowHeader";
+import RecommendedUserList from "@/_components/follow/RecommendedUserList";
+import { useRegisterStore } from "@/_store/register";
+import { RecommendedUser } from "@/_types/user/recommendedUser";
+import { getRecommendedSommelier } from "@/app/api/user/getRecommendedSommelier";
+import { useQuery } from "@tanstack/react-query";
+
+export default function Page() {
+  const { preferredAlcoholType } = useRegisterStore();
+  const {
+    data: recommendedSommelier,
+    isLoading: isLoadingRecommendedSommelier,
+    error,
+  } = useQuery<RecommendedUser[]>({
+    queryKey: ["recommendedSommelier"],
+    queryFn: getRecommendedSommelier,
+  });
+  if (isLoadingRecommendedSommelier) return <Loading />;
+  if (error) return <div>Error : {error.message}</div>;
+  if (recommendedSommelier) {
+    return (
+      <div className="w-full max-w-[560px]">
+        <FollowHeader title="팔로우하기" />
+        <div className="mx-[4%]">
+          <p className="text-base font-medium leading-6 text-cool-grayscale-800">
+            소믈리에 추천
+          </p>
+          <div className="text-sm leading-5 text-cool-grayscale-500">
+            <p>주라벨 서비스 내에서 인증을 통해 소믈리에</p>
+            <p>뱃지를 얻은 사람들이에요.</p>
+          </div>
+        </div>
+        <RecommendedUserList recommendedUserList={recommendedSommelier} />
+        <div className="mx-[4%]">
+          <p className="text-base font-medium text-cool-grayscale-800">
+            내 취향과 비슷한 유저들
+          </p>
+          <div className="text-sm text-cool-grayscale-500">
+            <span className="flex flex-row">
+              <p>선호하는 주종인</p>
+              <p className="mx-1 text-cool-grayscale-700">
+                {preferredAlcoholType.join(", ")}
+              </p>
+            </span>
+            <p>를 좋아하는 사람들이에요.</p>
+          </div>
+        </div>
+        <RecommendedUserList recommendedUserList={recommendedSommelier} />
+      </div>
+    );
+  }
+}
