fbfd78913f953a4c5824839bece6f1f2d41f37f1
Grit <68225058+Grit03@users.noreply.github.com>
2024-07-29 14:07:30 +0900

    Feat/share page (#4)
    * feat : 공유공간 1뎁스 구현
    
    - 프리텐다드 폰트설정
    - 전통주 시음 노트 1뎁스 구현
    - 전통주 일상생활 1뎁스 구현
    - 시음 노트, 일상생활 msw 구현
    - 무한 스크롤, 알림 숫자 미완료
    
    * fix : camelcase로 변경해서 오류 해결

---
+++ src/_common/PostList.tsx
@@ -0,0 +1,82 @@
+import { IPostList } from "@/_types/share";
+import { dateView } from "@/utils/time";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function PostList({
+  title,
+  content,
+  postHref,
+  username,
+  userImage,
+  contentThumbnail,
+  contentImageCount,
+  published,
+  likeCount,
+  commentCount,
+}: IPostList) {
+  return (
+    <Link href={postHref} className="block px-4 pt-5">
+      <div className="flex items-center gap-4">
+        <div>
+          <div className="mb-1 font-medium text-cool-grayscale-800">
+            {title}
+          </div>
+          <div className="mb-4 line-clamp-2 text-ellipsis text-sm text-cool-grayscale-700">
+            {content}
+          </div>
+        </div>
+
+        {contentThumbnail && (
+          <div className="relative h-[76px] w-[76px] shrink-0 overflow-hidden rounded-lg">
+            {contentImageCount && contentImageCount > 1 && (
+              <div className="absolute left-0 top-0 z-10 bg-black bg-opacity-60 px-2 py-0.5 text-sm text-white">
+                {contentImageCount}
+              </div>
+            )}
+            <Image
+              src={contentThumbnail}
+              alt="일상생활 썸네일 이미지"
+              fill
+              className="object-cover"
+            />
+          </div>
+        )}
+      </div>
+
+      <div className="flex items-center justify-between text-sm text-cool-grayscale-600">
+        <div className="flex items-center space-x-1">
+          <div className="relative h-6 w-6 overflow-hidden rounded-full">
+            <Image
+              src={userImage}
+              alt="작성자 이미지"
+              fill
+              className="object-cover"
+            />
+          </div>
+
+          <div>{username}</div>
+          <div className="text-cool-grayscale-400">&#183;</div>
+          <div>{dateView(published)}</div>
+        </div>
+        <div className="flex items-center">
+          <Image
+            src="/icons/like.png"
+            alt="좋아요 아이콘"
+            width={16}
+            height={16}
+          />
+          <div className="mr-1">{likeCount}</div>
+          <Image
+            src="/icons/comment.png"
+            alt="댓글 아이콘"
+            width={16}
+            height={16}
+          />
+          <div>{commentCount}</div>
+        </div>
+      </div>
+      <hr className="mt-5 bg-cool-grayscale-100" />
+    </Link>
+  );
+}
--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -2,9 +2,8 @@ export async function initMsw() {
   if (typeof window === "undefined") {
     const { server } = await import("../mocks/server");
     server.listen();
+  } else {
+    const { worker } = await import("../mocks/browser");
+    await worker.start();
   }
-  // else {
-  //   const { worker } = await import("../mocks/browser");
-  //   await worker.start();
-  // }
 }
