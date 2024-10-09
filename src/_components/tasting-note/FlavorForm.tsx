8b68cc956c030ed93be891a0a0dbbb2a9f425277
gaeguul <jeeham0226@gmail.com>
2024-10-10 01:42:45 +0900

    refactor: 미각 페이지 개선 그래프 값을 props로 넘기도록 구현

--- src/_components/tasting-note/FlavorForm.tsx
+++ src/_components/tasting-note/FlavorForm.tsx
@@ -1,7 +1,45 @@
 import BottomButton from "@/_common/BottomButton";
-import { useState } from "react";
+import { getFlavors } from "@/app/api/tasting-note/getTastingNoteFormInformation";
+import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
 import { useTastingNoteInformationStore } from "../../_store/tastingNote";
 import RadarChart from "./HexagonChart";
+
+interface FlavorInfo {
+  id: number;
+  name: string;
+}
+
+interface Level {
+  id: number;
+  score: number;
+  description: string;
+}
+
+interface FlavorLevelInfo {
+  flavor: FlavorInfo;
+  levels: Level[];
+}
+
+interface FlavorScore {
+  flavorId: number;
+  flavorName: string;
+  score: number;
+}
+
+interface IFlavorSelector {
+  flavorInfo: FlavorInfo;
+  levels: Level[];
+  onChange: () => void;
+}
+
+function FlavorSelector({ flavorInfo, levels, onChange }: IFlavorSelector) {
+  return (
+    <div className="flex flex-row">
+      <label className="text-base font-bold">{flavorInfo.name}</label>
+    </div>
+  );
+}
 
 interface IFlavorForm {
   handleStep: () => void;
@@ -9,14 +47,39 @@ interface IFlavorForm {
 
 export default function FlavorForm({ handleStep }: IFlavorForm) {
   const tastingNoteInformationStore = useTastingNoteInformationStore();
-  const [productName, setProductName] = useState("탁100 내추럴");
-  const [sweetness, setSweetness] = useState(1);
-  const [acidity, setAcidity] = useState(1);
-  const [bitterness, setBitterness] = useState(1);
-  const [umami, setUmami] = useState(1);
-  const [aftertaste, setAftertaste] = useState(1);
-  const [body, setBody] = useState(1);
-  const flavorData = [sweetness, acidity, bitterness, umami, aftertaste, body];
+  const {
+    alcoholicDrinksDetails: { alcoholicDrinksName },
+    alcoholTypeId,
+  } = tastingNoteInformationStore;
+
+  const [flavorScores, setFlavorScores] = useState<FlavorScore[]>([]);
+
+  // 맛 데이터 포인트 생성
+  const flavorChartData = flavorScores.map(({ flavorName, score }) => ({
+    label: flavorName,
+    data: score,
+  }));
+
+  const { data: flavorLevels } = useQuery<FlavorLevelInfo[]>({
+    queryKey: ["tastingNoteFlavors", alcoholTypeId],
+    queryFn: () => getFlavors(alcoholTypeId),
+  });
+
+  // flavorLevels를 기반으로 flavorScores 초기화
+  useEffect(() => {
+    if (flavorLevels) {
+      const initialScores = flavorLevels.map((flavorLevel) => ({
+        flavorId: flavorLevel.flavor.id,
+        flavorName: flavorLevel.flavor.name,
+        score: 0,
+      }));
+      setFlavorScores(initialScores);
+    }
+  }, [flavorLevels]);
+
+  useEffect(() => {
+    console.log("flavorLevels", flavorLevels);
+  }, [flavorLevels]);
 
   const handleNextButton = () => {
     // localStorage에 맛 저장
@@ -24,20 +87,30 @@ export default function FlavorForm({ handleStep }: IFlavorForm) {
   };
 
   return (
-    <>
+    <div className="mx-[18px] mt-6 flex flex-col gap-y-10 pb-[102px]">
       {/* 타이틀 */}
       <div>
         <p className="text-xl font-bold text-cool-grayscale-800">
-          <span className="text-primary-700">{productName}</span>의 맛은
+          <span className="text-primary-700">{alcoholicDrinksName}</span>의 맛은
           어떤가요?
         </p>
       </div>
       <div className="flex justify-center">
-        <RadarChart data={flavorData} />
+        <RadarChart dataPoints={flavorChartData} />
+      </div>
+      <div className="flex flex-col gap-y-6">
+        {/* 맛의 정도 선택 */}
+        {flavorLevels?.map((flavorLevel) => (
+          <FlavorSelector
+            flavorInfo={flavorLevel.flavor}
+            levels={flavorLevel.levels}
+            onChange={() => {}}
+          />
+        ))}
       </div>
       <BottomButton enableButton={true} onClick={handleNextButton}>
         다음
       </BottomButton>
-    </>
+    </div>
   );
 }
