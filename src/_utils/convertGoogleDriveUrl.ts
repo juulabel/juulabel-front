77d6c500784421d9923fdfc2273b6a177d8f5599
Youngjun Kim <158126791+Youngjun97@users.noreply.github.com>
2025-03-02 18:58:54 +0900

    chore: Update .gitignore and improve Google Drive URL conversion error handling

--- src/_utils/convertGoogleDriveUrl.ts
+++ src/_utils/convertGoogleDriveUrl.ts
@@ -4,12 +4,15 @@ export default function convertGoogleDriveURL(driveUrl: string) {
   try {
     const match = driveUrl.match(/(?:id=|\/d\/)([a-zA-Z0-9_-]+)/);
     if (match && match[1]) {
-      return `https://drive.google.com/thumbnail?id=${driveUrl.match(/(?:id=|\/d\/)([a-zA-Z0-9_-]+)/)![1]}`;
+      return `https://drive.google.com/thumbnail?id=${match[1]}`;
     } else {
       throw new Error("Invalid Google Drive URL");
     }
-  } catch (error: any) {
-    console.error("Error:", error.message);
+  } catch (error) {
+    console.error(
+      "Error:",
+      error instanceof Error ? error.message : String(error),
+    );
     return `${process.env.NEXT_PUBLIC_IMAGE_BASE_PATH}/images/placeholders/alcohols/${placeholderThumbnailProvider("기타주류")}.png`;
   }
 }
