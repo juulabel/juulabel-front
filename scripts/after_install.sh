ffc7b15b02ce2b6186efcd3ab00a8ba80dd81479
ohamin26 <ohamin26@gmail.com>
2025-05-17 19:12:25 +0900

    feat : 배포 자동화 (qa 서버 테스트)

---
+++ scripts/after_install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+echo "=== [PM2 restart] Restart juulabel-front process ==="
+
+ENV_VERSION=$(grep ENV_VERSION /home/juulabel-front/.env | cut -d '=' -f2)
+
+if [ "$ENV_VERSION" == "dev" ]; then
+  echo "=== [PM2 restart] Restart juulabel-front-qa process ==="
+  rm -rf /home/juulabel-qa/juulabel-front
+  mv /home/juulabel-front /home/juulabel-qa/juulabel-front
+  pm2 stop juulabel-front-qa
+  cd /home/juulabel-qa/juulabel-front
+  pm2 start npm --name juulabel-front-qa -- run start:qa
+  pm2 reload juulabel-front-qa
+elif [ "$ENV_VERSION" == "production" ]; then
+  echo "=== [PM2 restart] Restart juulabel-front process ==="
+  rm -rf /home/juulabel/juulabel-front
+  rm -rf /home/juulabel-backup/juulabel-front
+  cp /home/juulabel-front /home/juulabel-backup/juulabel-front
+  mv /home/juulabel-front /home/juulabel-qa/juulabel-front
+  pm2 stop juulabel-front
+  cd /home/juulabel/juulabel-front
+  pm2 start npm --name juulabel-front -- run start
+  pm2 reload juulabel-front
+fi
