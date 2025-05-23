9e8afbd09c503236a4c6da97a5a7e2aa9d240115
ohamin26 <ohamin26@gmail.com>
2025-05-24 01:26:14 +0900

    fix : after_install 패키지 설치 pm2 실행 명령어 ubuntu 권한 실행으로 변경

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,31 +5,45 @@ set -e
 echo "=== [PM2 restart] Restart juulabel-front process ==="
 
 ENV_VERSION=$(grep ENV_VERSION /home/juulabel-front/.env | cut -d '=' -f2)
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-export PATH="/home/ubuntu/.nvm/versions/node/v22.4.0/bin:$PATH" 
-export PM2_HOME="$HOME/.pm2"
+
+PM2_SCRIPT=$(cat <<EOF
+  export NVM_DIR="/home/ubuntu/.nvm"
+  [ -s "\$NVM_DIR/nvm.sh" ] && . "\$NVM_DIR/nvm.sh"
+  export PATH="/home/ubuntu/.nvm/versions/node/v22.4.0/bin:\$PATH"
+  export PM2_HOME="/home/ubuntu/.pm2"
+EOF
+)
 
 if [ "$ENV_VERSION" == "dev" ]; then
   echo "=== [PM2 restart] Restart juulabel-front-qa process ==="
-  sudo rm -rf /home/juulabel-qa/juulabel-front
-  sudo cp -r /home/juulabel-front /home/juulabel-qa/juulabel-front
-  cd /home/juulabel-qa/juulabel-front
-  pnpm install
-  pm2 delete front-qa
-  pm2 start npm --name front-qa -- run start:qa
+
+  rm -rf /home/juulabel-qa/juulabel-front
+  cp -r /home/juulabel-front /home/juulabel-qa/juulabel-front
+
+  sudo -u ubuntu -H bash -c "
+    $PM2_SCRIPT
+    cd /home/juulabel-qa/juulabel-front
+    pnpm install
+    pm2 delete front-qa || true
+    pm2 start npm --name front-qa -- run start:qa
+  "
+
 elif [ "$ENV_VERSION" == "production" ]; then
   echo "=== [PM2 restart] Restart juulabel-front process ==="
-  sudo rm -rf /home/juulabel/juulabel-front
-  sudo rm -rf /home/juulabel-backup/juulabel-front
+
+  rm -rf /home/juulabel/juulabel-front
+  rm -rf /home/juulabel-backup/juulabel-front
   cp -r /home/juulabel-front /home/juulabel-backup/juulabel-front
   cp -r /home/juulabel-front /home/juulabel/juulabel-front
-  cd /home/juulabel/juulabel-front
-  pnpm install
-  pm2 delete front-live
-  pm2 start npm --name front-live -- run start
+
+  sudo -u ubuntu -H bash -c "
+    $PM2_SCRIPT
+    cd /home/juulabel/juulabel-front
+    pnpm install
+    pm2 delete front-live || true
+    pm2 start npm --name front-live -- run start
+  "
 fi
 
-sudo rm -rf /home/juulabel-front
-sudo mkdir /home/juulabel-front
+rm -rf /home/juulabel-front
+mkdir /home/juulabel-front
